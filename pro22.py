60ca7a675d07156fe541fcef594334ec733e5bf0
jagdishmahale05 <jagdishmahale05@gmail.com>
2025-04-14 21:37:26 +0530

    Add files via upload
    This is a simple Python script that takes two integer inputs from the user, calculates their sum, and checks if the result is positive, negative, or zero. It’s a basic example for beginners to understand user input, arithmetic operations, and conditional logic.

---
+++ pro22.py
@@ -0,0 +1,9 @@
+value1 = int(input("enter the value 1"))
+value2 = int(input("enter the value 2"))
+sum = value2+value1
+if sum>0:
+    print("sum is greater than 0")
+elif sum<0:
+    print("sum is less than 0")
+else:
+    print("sum is equal to 0")
